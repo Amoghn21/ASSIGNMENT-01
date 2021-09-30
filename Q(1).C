dd55080cb13dc3569a8539a30117cbdc741df0f4
Amoghn21 <88525606+Amoghn21@users.noreply.github.com>
2021-09-30 15:22:27 +0530

    Update Q(1).C

--- Q(1).C
+++ Q(1).C
@@ -1,1 +1,31 @@
+#include<stdio.h>
+int main()
+{  char num[500];
+   printf("enter the number\n");
+   gets(num);
+   int x,count=0;
+   for(int i=0;num[i]!='\0';i++)
+   {  if(num[i]=='.') 
+      count=1;
+   }
+   if(count!=1)
+   printf("invalid");
+   else 
+   { 
+    for(int i=0;num[i]!=0;i++)
+     {    if(num[i]=='.')
+            x=i;
+     } 
+     if(num[x+1]!='\0' && num[x-1]!='\0')
+        printf("valid");
+     else 
+        printf("invalid");
+     
+    }
+return 0;
+ }   
+
+   
+    
+
 
